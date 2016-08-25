4b20185cc75bcc0f9950d8d5eacb3a79ab39a068
Rohit Gupta <rohit.gupta@eurecom.fr>
2016-08-25 14:11:23 +0200

    minor fix to compute usrp timing based on nsamps passed to function

--- targets/ARCH/USRP/USERSPACE/LIB/usrp_lib.cpp
+++ targets/ARCH/USRP/USERSPACE/LIB/usrp_lib.cpp
@@ -205,7 +205,7 @@ static int trx_usrp_write(openair0_device *device, openair0_timestamp timestamp,
     time_avg=(time_diff+time_avg) /2.0;
 
    //prints statics of uhd every 10 seconds
-   if ( loop % (10 * ((int)device->openair0_cfg[0].sample_rate /(int)device->openair0_cfg[0].samples_per_packet )) ==0)
+   if ( loop % (10 * ((int)device->openair0_cfg[0].sample_rate /(int)nsamps )) ==0)
      LOG_I(HW,"usrp_write: min(ns)=%d, max(ns)=%d, avg(ns)=%d\n", (int)time_min, (int)time_max,(int)time_avg);
 
    loop++;
@@ -322,7 +322,7 @@ static int trx_usrp_read(openair0_device *device, openair0_timestamp *ptimestamp
     time_avg=(time_diff+time_avg) /2.0;
 
   //prints statics of uhd every 10 seconds
-  if ( loop % (10 * ((int)device->openair0_cfg[0].sample_rate /(int)device->openair0_cfg[0].samples_per_packet )) ==0)
+  if ( loop % (10 * ((int)device->openair0_cfg[0].sample_rate /(int)nsamps )) ==0)
      LOG_I(HW,"usrp_read: min(ns)=%d, max(ns)=%d, avg(ns)=%d\n", (int)time_min, (int)time_max,(int)time_avg);
 
   loop++;
